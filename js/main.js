aa1f6bcab32563e9475720d0fca2b86a66e4a280
Victor Schweitz <victorschweitz@hotmail.com>
2024-11-14 19:36:44 +0100

    Search container set up

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const searchMealInput = document.querySelector('.meal-search-input')
+const searchMealInputToggleBtn = document.querySelector('.search-meal-input-toggle-btn')
+
+
+// EVENT LISTENERS //
+searchMealInputToggleBtn.addEventListener('click', toggleSearchMealInput)
+
+// FUNCTIONS //
+function toggleSearchMealInput()
+{
+    searchMealInput.classList.toggle('active')
+    searchMealInputToggleBtn.classList.toggle('active')
+}
